583278f5c44d71d0ddfe7946d77296d9f384650c
mathiasbraaten <mathias.braten00@gmail.com>
2025-02-11 16:04:49 +0100

    fylker

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -6,6 +6,10 @@ import { useGeographic } from "ol/proj";
 
 // Styling of OpenLayers components like zoom and pan controls
 import "ol/ol.css";
+import VectorLayer from "ol/layer/Vector";
+import { GeoJSON } from "ol/format";
+import VectorSource from "ol/source/Vector";
+import { Stroke, Style } from "ol/style";
 
 // By calling the "useGeographic" function in OpenLayers, we tell that we want coordinates to be in degrees
 //  instead of meters, which is the default. Without this `center: [10.6, 59.9]` brings us to "null island"
@@ -14,15 +18,26 @@ useGeographic();
 // Here we create a Map object. Make sure you `import { Map } from "ol"`. Otherwise, the standard Javascript
 //  map data structure will be used
 const map = new Map({
-  // The map will be centered on a position in longitude (x-coordinate, east) and latitude (y-coordinate, north),
-  //   with a certain zoom level
-  view: new View({ center: [10.8, 59.9], zoom: 13 }),
-  // map tile images will be from the Open Street Map (OSM) tile layer
-  layers: [new TileLayer({ source: new OSM() })],
+  view: new View({ center: [10.8, 59.9], zoom: 9 }),
+  layers: [
+    new TileLayer({ source: new OSM() }),
+    new VectorLayer({
+      source: new VectorSource({
+        url: "/oppgave2/geojson/fylker.json",
+        format: new GeoJSON(),
+      }),
+      style: new Style({
+        stroke: new Stroke({
+          color: "blue",
+          width: 2,
+        }),
+      }),
+    }),
+  ],
 });
 
 // A functional React component
-export function Application() {
+export default function Application() {
   // `useRef` bridges the gap between JavaScript functions that expect DOM objects and React components
   const mapRef = useRef<HTMLDivElement | null>(null);
   // When we display the page, we want the OpenLayers map object to target the DOM object refererred to by the
@@ -31,6 +46,6 @@ export function Application() {
     map.setTarget(mapRef.current!);
   }, []);
 
-  // This is the location (in React) where we want the map to be displayed
+  // This is the location (in React) where we want the\ map to be displayed
   return <div ref={mapRef}></div>;
 }
